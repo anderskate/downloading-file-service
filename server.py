1d79dbab9eaf810aea19ae81a918143b2dabeb6c
Andrey Starozhilov <andrey.starozhilov@saritasa.com>
2020-09-15 13:06:11 +0500

    add ability to download zip

--- server.py
+++ server.py
@@ -1,9 +1,40 @@
 from aiohttp import web
 import aiofiles
+import asyncio
+
+
+ZIP_FILE_NAME = 'archive.zip'
+PHOTOS_DIRECTORY = 'test_photos'
+
+# Maximum file fragment size to be returned to the user
+MAX_FILE_FRAGMENT_SIZE = 100000
 
 
 async def archivate(request):
-    raise NotImplementedError
+    """Zip the desired directory and return to the user"""
+    response = web.StreamResponse()
+
+    # Set required headers
+    response.headers['Content-Type'] = 'text/html'
+    response.headers['Content-Disposition'] = \
+        f'attachment; filename={ZIP_FILE_NAME}'
+
+    # Send HTTP headers to the client
+    await response.prepare(request)
+
+    archive_hash = request.match_info.get('archive_hash')
+    proc = await asyncio.create_subprocess_shell(
+        f'cd {PHOTOS_DIRECTORY}/ && zip -r - {archive_hash}',
+        stdout=asyncio.subprocess.PIPE,
+    )
+    while True:
+        chunk = await proc.stdout.read(n=MAX_FILE_FRAGMENT_SIZE)
+
+        if chunk == b'':
+            return response
+
+        # Send another portion of the response to the client
+        await response.write(chunk)
 
 
 async def handle_index_page(request):
